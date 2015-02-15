a6555399ef3b2215da45f76e6fc5f1269b098174
Romain Porte <microjoe@mailoo.org>
2015-02-15 10:53:50 +0100

    Added some checks for fresh installs

--- vim/vim/install.sh
+++ vim/vim/install.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# Install script for vim plugins.
+
 function cloneGithub {
 	# Use your preferred retrieve method (git clone or zip archive) here
+
+	#cloneGithubGit "$1"
 	cloneGithubZip "$1"
 }
 
+function checkPrg {
+	hash "$1" 2>/dev/null || {
+		echo >&2 "$1 is not installed."
+		echo >&2 "Please install $1 or change installation method in" \
+			"the script."
+		exit 1
+	}
+}
+
 function cloneGithubGit {
+	checkPrg git
+
     echo "Cloning $1..."
     git clone https://github.com/$1
 }
 
 function cloneGithubZip {
+	checkPrg unzip
+
 	url="https://github.com/$1/archive/master.zip"
 	repo=$(expr match "$1" '.*/\(.*\)')
 
@@ -24,6 +41,8 @@ function cloneGithubZip {
 
 echo "Creating directories..."
 mkdir -p ~/.vim/bundle && mkdir -p ~/.vim/autoload
+
+checkPrg curl
 
 echo "Installing pathogen..."
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
